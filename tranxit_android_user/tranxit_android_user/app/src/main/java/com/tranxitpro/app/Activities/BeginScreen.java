0e3a50615d53610dfc04ba864ce5ff997a2a2948
hakkempalakkal <hakkempalakkal@gmail.com>
2021-03-26 14:54:40 +0400

    first push

---
+++ tranxit_android_user/tranxit_android_user/app/src/main/java/com/tranxitpro/app/Activities/BeginScreen.java
@@ -0,0 +1,55 @@
+package com.tranxitpro.app.Activities;
+
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.splunk.mint.Mint;
+import com.tranxitpro.app.R;
+
+public class BeginScreen extends AppCompatActivity {
+
+    TextView enter_ur_mailID;
+    LinearLayout social_layout, lnrBegin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mint.setApplicationEnvironment(Mint.appEnvironmentStaging);
+        Mint.initAndStartSession(this.getApplication(), "3c1d6462");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_begin);
+        enter_ur_mailID = (TextView)findViewById(R.id.enter_ur_mailID);
+        social_layout = (LinearLayout) findViewById(R.id.social_layout);
+        lnrBegin = (LinearLayout) findViewById(R.id.lnrBegin);
+        enter_ur_mailID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(BeginScreen.this, ActivityEmail.class);
+                startActivity(mainIntent);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+            }
+        });
+        social_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(BeginScreen.this, ActivitySocialLogin.class);
+                startActivity(mainIntent);
+                overridePendingTransition(R.anim.slide_in, R.anim.slide_out);
+            }
+        });
+
+    }
+
+
+}
